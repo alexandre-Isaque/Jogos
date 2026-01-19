d2754adc8e96710bfbe89360f354477e9b0873ed
alexandre-Isaque <xandisaque@gmail.com>
2026-01-19 17:30:52 -0300

    movimentacao boa

--- Snake/main.js
+++ Snake/main.js
@@ -2,29 +2,28 @@
 const start = document.getElementById("start");
 const exit = document.getElementById("exit");
 const game = document.getElementById("game");
-const ponts = document.getElementById("pontos");
+const color = document.getElementById("color");
 
 // canvas
 const canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
 // tamanho do canvas
-const HEIGHT = 600;
-const WEIGHT = 600;
+let HEIGHT = 600;
+let WEIGHT = 600;
 
 // velocidade do snake
-let speed = 1;
+let speed = 5;
+
+// cor do snake
+let snake_Color;
 
 // tamanho do snake
 let SNAKE_WEIGHT = 10;
 let SNAKE_HEIGHT = 5;
 
-// pontos
-let point = 0;
-ponts.innerHTML = point;
-
 // aumeta o tamanho do snake
-const snakeAdd = 10;
+const snakeAdd = 5;
 
 // posição aleatoria da comida
 let foodPosition = {
@@ -33,22 +32,51 @@ let foodPosition = {
 }
 
 // posição aleatoria do snake
-let snakePosition = {
-    x: Math.floor(Math.random() * canvas.width),
-    y: Math.floor(Math.random() * canvas.height)
+let snakeHead = [
+    {
+        x: 100, y: 100,
+        x: 100, y: 100,
+        x: 100, y: 100
+    }
+];
+
+let snake = {
+    x: Math.ceil(Math.random() * canvas.width),
+    y: Math.ceil(Math.random() * canvas.height),
+    size: 20
+}
+
+let snakeBody = { x: 1, y: 0};
+
+
+function update(){
+    color.style.display = "flex";
+    color.innerHTML = "mudar de cor";
+
+    for(let i = 0; i < 0; i++){
+        snake.x = snake.x - 1;
+        snake.y = snake.y - 1;
+    }
+
+    // move a cabeça
+    snake.x += snakeBody.x * speed;
+    snake.y += snakeBody.y * speed;
+
+    // funçoes
+    drawSnake();
+    drawFood();
+    collition();
+    myCanvas();
+    colors_snakes();
 }
 
 // inicia o jogo
-function startGame(){
-    start.addEventListener('click', () => {
-        // intervalo
-        setInterval(() => {
-            food();
-            collition();
-            myCanvas();
-        }, 100);
-    });
-}
+start.addEventListener('click', () => {
+    // intervalo
+    setInterval(() => {
+        update();
+    }, 1000);
+});
 
 // deixa o canvas visivel
 function myCanvas(){
@@ -56,69 +84,58 @@ function myCanvas(){
     canvas.style.display = "flex";
     canvas.style.height = `${HEIGHT}px`;
     canvas.style.width = `${WEIGHT}px`;
-    // adiciona pontuacao
-    ponts.style.display = "flex";
-
     // tira o primeiro layout
     game.style.display = "none";
 }
 
 // colisao
 function collition(){
-
-    if( snakePosition.x < canvas.width ||
-        snakePosition.x > canvas.width ||
-        snakePosition.y < canvas.height ||
-        snakePosition.y > canvas.height )
-    {
-        // remove o canvas
-        canvas.style.display = "none";
-        canvas.style.height = "0px";
-        canvas.style.width = "0px";
-
-        // volta ao primeiro layout
-        game.style.display = "flex";
-
-    }
-
-    if(snakePosition.x == foodPosition.x && snakePosition.y == foodPosition.y){
+    if(snakeHead.x == foodPosition.x && snakeHead.y == foodPosition.y){
         // aumenta a altura, largura
-        SNAKE_WEIGHT += snakeAdd;
+        snake.size += snakeAdd;
         // adiciona pontos ao jogador
         ponts.innerHTML += 1;
         // aumenta a velocidade
-        speed += 1;
+        speed += speed;
         // muda a posicao da comida
         foodPosition.x = Math.ceil(Math.random() * canvas.width);
         foodPosition.y = Math.ceil(Math.random() * canvas.height);
+
+        // limpa o canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    if( snake.x > canvas.width || snake.x < canvas.width ||
+        snake.y < canvas.height || snake.y > canvas.height
+    ){
+
+        console.log("perdeu");
     }
 }
 
 // food == comida
-function food(){
+function drawFood(){
     ctx.fillStyle = "red";
-    ctx.fillRect(foodPosition.x, foodPosition.y, 10, 5);
+    ctx.fillRect(foodPosition.x, foodPosition.y, 20, 10);
 }
 
 // snake == cobra
-function snake(){
-    ctx.fillStyle = "green";
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillRect(snakePosition.x, snakePosition.y, SNAKE_WEIGHT, SNAKE_HEIGHT);
+function drawSnake(){
+    ctx.fillStyle = snake_Color;
+    ctx.fillRect(snake.x, snake.y, snake.size, 10);
 }
 
 // teclado
 document.addEventListener('keypress', (event) => {
-    // keys
-    if(event.key == 'a') snakePosition.x -= speed;
-    if(event.key == 's') snakePosition.y += speed;
-    if(event.key == 'd') snakePosition.x += speed;
-    if(event.key == 'w') snakePosition.y -= speed;
-
-    // desenha o snake, comida de novo
-    snake();
-    food();
+    if(event.key === "w" && snakeBody.y !==  1) snakeBody = { x: 0, y: -1};
+    if(event.key === "s" && snakeBody.y !== -1) snakeBody = { x: 0, y: 1};
+    if(event.key === "a" && snakeBody.x !== -1) snakeBody = { x: -1, y: 0};
+    if(event.key === "d" && snakeBody.y !== 1) snakeBody = {x: 1, y: 0};
 });
 
-// executa o jogo
-startGame();
+// color
+function colors_snakes(){
+
+};
+
+
